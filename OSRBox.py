5e5e3749f92dc52cb234acf90bbee52258a2429f
Florian Indot <Florian Indot>
2017-02-16 07:13:10 +0100

    Morning work, still not able to initialize communication.

--- OSRBox.py
+++ OSRBox.py
@@ -8,13 +8,14 @@ import codecs
 class OSRBox:
 
     port = 'COM3'
+
+    initial_baudrate = 9600
     baudrate = 19200
 
     tx_encoding = 'UTF-8'
-    rx_encoding = 'Latin1'
 
-    menu_char = '0x14'
-    exit_char = '0x1d'
+    exit_char = 0x1D
+    menu_char = 0x14
 
     '''
     Class constructor.
@@ -28,12 +29,12 @@ class OSRBox:
         self.port       = self.port if port is False else port
         self.baudrate   = self.baudrate if baudrate is False else baudrate
 
-        self.tx_encoder = codecs.getincrementalencoder( self.tx_encoding )
+        self.tx_encoder = codecs.getincrementalencoder( self.tx_encoding )()
 
         self.ser = serial.Serial()
 
         self.ser.port       = self.port
-        self.ser.baudrate   = self.baudrate
+        self.ser.baudrate   = self.initial_baudrate
 
         try:
 
@@ -46,16 +47,33 @@ class OSRBox:
 
 
     '''
-    Starts the communication by sending a carriage return (?) to the OSRBox.
+    Starts the communication by sending the escape sequence to the OSRBox.
     '''
     def start( self ):
 
-        print( "Starting..." )
-        self.write( self.menu_char + self.exit_char )
+        # self.ser.write( str(0x1D) + str(0x14) )
+        # self.ser.write( '\x0A' )
+        # self.ser.write( '\x0C' )
+        # self.ser.write( self.menu_char )
+
+        self.ser.baudrate = self.baudrate
+        print( "Starting communication on " + self.port + " at " + str( self.baudrate ) + "." )
+
+    '''
+    Ends the communication by sending the escape sequence to the OSRBox.
+    '''
+    def exit( self ):
+
+        self.ser.write( self.menu_char )
+        self.ser.write( self.exit_char )
+
+        self.ser.close()
+
 
     def write( self, data ):
 
         self.ser.write( self.tx_encoder.encode( data ) )
+
 
     '''
     Reads the current OSRBox state.
@@ -72,9 +90,29 @@ class OSRBox:
             return False
 
         response = math.log( response, 2 )
-        response = int( response )
+        response = int( response ) + 1
 
         return response
+
+    '''
+    WIP
+    '''
+    def input( self ):
+
+        on = True
+
+        input_char = ''
+        last_input_char = ''
+
+        while on:
+
+            if ( self.ser.inWaiting ):
+
+                input_char = self.read()
+                if ( input_char != False ):
+
+                    last_input_char = input_char
+                    print input_char
 
     '''
     Outputs the current input flow, visual purposes.
@@ -86,8 +124,9 @@ class OSRBox:
             if ( self.ser.inWaiting() ):
                 print self.read()
 
+
 if __name__ == "__main__":
 
     pad = OSRBox()
     pad.start()
-    pad.debug()
+    pad.input()
