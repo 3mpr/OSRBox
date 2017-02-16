fc94bb11091357822be85c36aa7df3858afa7e0c
Florian Indot <Florian Indot>
2017-02-16 15:49:54 +0100

    Second iteration, with proper initialization sequence and responsibilites division.

---
+++ OSRBoxDriver.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import OSRBoxWrapper
+import keyboard
+import threading
+import time
+
+class OSRBoxDriver:
+
+    nb_keys = 5
+
+    '''
+    Class Constructor.
+
+    Initializes a few important variables, such as COM port, baudrate and
+    emulated keys.
+    '''
+    def __init__ ( self, port ):
+
+        self.pad = OSRBoxWrapper.OSRBoxWrapper( port, 19200 )
+
+        self.emulated_keys = {
+            1 : 'a',
+            2 : 'z',
+            3 : 'e',
+            4 : 'r',
+            5 : 't'
+        }
+
+        self._term = threading.Thread( target = self.term, name = 'term' )
+        self._reader = threading.Thread( target = self.reader, name = 'rx' )
+
+
+    '''
+    Destructor redifinition.
+    '''
+    def __del__( self ):
+
+        self.pad.close()
+        self.alive = False
+
+
+    '''
+    Binds one of the OSRBox numerical keys to emulate a given keyboard key.
+
+    @param int      nb  The pressed OSRBox key
+    @param string   key The emulated key description
+    '''
+    def bind( self, nb, key):
+
+        if ( nb > self.nb_keys ):
+            raise ValueError( 'There is only 5 keys on the OSRBox!' )
+
+        self.emulated_keys[nb] = key
+
+
+    '''
+    Keeps track of the current OSRBox pressed key and emulate keyboard keys
+    accordingly.
+    '''
+    def reader( self ):
+
+        last_key_pressed = False
+
+        while self.alive:
+            key_pressed = self.pad.read()
+
+            if( key_pressed is not False ):
+
+                for key in self.emulated_keys:
+                    if key_pressed == key and key_pressed != last_key_pressed:
+                        last_key_pressed = key_pressed
+                        self.current_key = key_pressed
+                        keyboard.press( self.emulated_keys[key] )
+
+            elif last_key_pressed:
+
+                keyboard.release( self.emulated_keys[last_key_pressed] )
+                last_key_pressed = False
+
+            # time.sleep( 0.1 )
+
+
+    '''
+    Runs two thread, one is a miniterm and let the user ends the program,
+    the other keeps updated the current active key.
+    '''
+    def run( self ):
+
+        self.pad.open()
+
+        self.alive = True
+
+        self._reader.start()
+        self._term.start()
+
+        self._reader.join()
+        self._term.join()
+
+    '''
+    Ends the process.
+    '''
+    def stop( self ):
+
+        self.alive = False
+
+
+    '''
+    Exit hook.
+    '''
+    def term( self ):
+
+        while self.alive:
+            exit = raw_input( 'Press Q to stop the OSRBox...' )
+
+            if exit == 'Q':
+                self.alive = False
+
+
+
+if __name__=='__main__':
+
+    drv = OSRBoxDriver('COM3')
+    drv.run()
---
+++ OSRBoxWrapper.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import math
+import time
+import serial
+import codecs
+
+class OSRBoxWrapper:
+
+    '''
+    Class constructor.
+    Initializes a few importants variables.
+
+    @param string   port        The port to connect to
+    @param int      baudrate    The baudrate ~= the speed at which the communication will take place
+    '''
+    def __init__( self, port, baudrate ):
+
+        if port is None:
+            raise ValueError( 'No COM port specified.' )
+
+        self.port       = port
+        self.baudrate   = baudrate
+
+        self.tx_encoder = codecs.getincrementalencoder( 'UTF-8' )()
+
+        self.ser = serial.Serial()
+
+        self.ser.port       = self.port
+        self.ser.baudrate   = self.baudrate
+
+        self.escape_sequence = '\x1D\x14\n\r'
+
+    '''
+    Starts the communication by sending the escape sequence to the OSRBox.
+    '''
+    def open( self ):
+
+        print( "\nStarting communication on " + self.port + " at " + str( self.baudrate ) + " bauds.\n" )
+
+        try:
+
+            self.command( self.escape_sequence )
+
+        except serial.serialutil.SerialException:
+
+            print( 'Unable to open port ' + self.port + '. Exiting...' )
+            raise
+
+
+    '''
+    Ends the communication by sending the escape sequence to the OSRBox.
+    '''
+    def close( self ):
+
+        # Does'nt work !
+        self.command( self.escape_sequence )
+        self.ser.close()
+
+
+    '''
+    Downgrades the connection rate to 9600 bauds, issues the command to the OSR
+    then reupgrade the connection to 19200 bauds.
+
+    @param string cmd The command to issue
+    '''
+    def command( self, cmd ):
+
+        self.ser.close()
+        self.ser.baudrate = 9600
+        self.ser.open()
+        self.write( cmd )
+        self.ser.close()
+        self.ser.baudrate = self.baudrate
+        self.ser.open()
+
+
+    '''
+    Wraps the serial writing operation with an UTF-8 encoding.
+
+    @param string Data The data to write
+    '''
+    def write( self, data ):
+
+        self.ser.write( self.tx_encoder.encode( data ) )
+
+
+    '''
+    Reads the current OSRBox state.
+    Can be False, 1, 2, 3, 4, 5 where False stands for no button(s) pressed(s)
+    and the numbers for the correlating key number (1-indexed)
+
+    @return int key The pressed key or False if no key is currently pressed
+    '''
+    def read( self ):
+
+        response = self.ser.read( 1 )
+        response = ord( response )
+
+        if response == 0:
+            return False
+
+        response = math.log( response, 2 )
+        response = int( response ) + 1
+
+        return response
+
+    #def input( self ):
+
+    #    on = True
+
+    #    input_char = ''
+    #    last_input_char = ''
+
+    #    while on:
+
+    #        if ( self.ser.inWaiting ):
+
+    #            input_char = self.read()
+    #            if ( input_char != False ):
+
+    #                last_input_char = input_char
+    #                print input_char
+
+    '''
+    Outputs the current input flow, visual purposes.
+    '''
+    def debug( self ):
+
+        while True:
+
+            if ( self.ser.inWaiting() ):
+                print self.read()
